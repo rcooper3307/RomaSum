df3604e29392de7c4a973f65fd570bdfa16d75b7
rcooper3307 <rcooper3307@bths.edu>
2018-10-12 08:17:42 -0400

    i made a mistake last time here's the Real Deal.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("The sum is "+Main.sumFifteen(15));
-    }
-    public static int sumFifteen(int num){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number.");
+        int num = input.nextInt();
         int sum = 0;
         for(int i = 1; i <= num; i++){
             sum = sum + i;
         }
-        return sum;
+        System.out.println("The sum of 1 to "+num+" is "+sum);
     }
 }
